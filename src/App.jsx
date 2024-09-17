9a94d7108939e85a27fb90c1a4325de336c5ff55
minorscarf <minorscarf@gmail.com>
2024-09-18 02:22:38 +0900

    station5までやった

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 // DO NOT DELETE
 
+import { useState } from 'react';
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
+
 export const App = () => {
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const [dogUrl, setDogUrl] = useState(url);
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <header>
+      イッヌ
+      <img src={dogUrl}/>
+      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/appenzeller/n02107908_3155.jpg")}></button>
+    </header>
   )
 }
